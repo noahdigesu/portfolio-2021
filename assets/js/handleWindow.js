5324841757646a9695a53f4dd7e323d70b155ab1
Noah Di Gesu <54229@etu.he2b.be>
2021-06-13 13:12:26 +0200

    Ajout bottom-bar

--- assets/js/handleWindow.js
+++ assets/js/handleWindow.js
@@ -1,27 +1,64 @@
 $(".window").mousedown(function () {
-    let highestIndex = 0, zIndex = 0;
-    $(".window").each(function () {
-        $(this).attr("toggled", "false");
+	let highestIndex = 0,
+		zIndex = 0;
+	$(".window").each(function () {
+		$(this).attr("toggled", "false");
 
-        zIndex = parseInt($(this).css("z-index"));
-        if (zIndex > highestIndex) highestIndex = zIndex;
-    });
-    $(this).attr("toggled", "true");
-    $(this).css("z-index", highestIndex + 1);
+		zIndex = parseInt($(this).css("z-index"));
+		if (zIndex > highestIndex) highestIndex = zIndex;
+	});
+	$(this).attr("toggled", "true");
+	$(this).css("z-index", highestIndex + 1);
 });
 
-$(".close").click(function(){
-    $(this).parent().parent()
-        .addClass("animate__animated animate__zoomOut animate__fast")
-        .delay(400).queue(function(){
-            $(this).remove();
-        });
-    
+$(".close").click(function () {
+	$(this)
+		.parent()
+		.parent()
+		.addClass("animate__animated animate__zoomOut animate__fast")
+		.delay(400)
+		.queue(function () {
+			$(this).remove();
+		});
 });
 
-$(".minimize").click(function(){
-    $(this).parent().parent().addClass("animate__animated animate__zoomOutDown animate__faster");
-    /* setTimeout(function() {
-        $(this).parent().parent().css("display", "none");
-    }, 500); */
+$(".minimize").click(function () {
+	$(this)
+		.parent()
+		.parent()
+		.addClass("animate__animated animate__zoomOutDown animate__faster");
+	addToBottomBar($(this).parent().parent());
+});
+
+const addToBottomBar = (window) => {
+	$(".bottom-bar").attr("toggled", "true");
+
+	let windowId = window.attr("id");
+	setTimeout(function () {
+		$(".bottom-bar").append(
+			`<span id='${windowId}' class='icon animate__animated animate__fadeInUp animate__faster'>` +
+				`<img src='../../images/icons/${windowId}.svg' alt='icon'/>` +
+				"</span>"
+		);
+	}, 300);
+};
+
+$(".bottom-bar").on("click", ".icon", function () {
+	$(`.window#${this.id}`).removeClass("animate__zoomOutDown");
+	$(`.window#${this.id}`).addClass("animate__zoomInUp");
+
+	$(this).removeClass("animate__fadeInUp");
+	$(this).addClass("animate__fadeOutDown");
+
+	$(this)
+		.delay(400)
+		.queue(function () {
+			$(this).remove();
+		});
+
+	if ($(this).parent().children().length === 1) {
+		setTimeout(function () {
+			$(".bottom-bar").attr("toggled", "false");
+		}, 200);
+	}
 });
--- assets/js/work.js
+++ assets/js/work.js
@@ -1,19 +1,33 @@
 $('.project').click(function() {
-    $('.project').each(function(){
+    toggleProjectsOff(this);
+    toggleProjectOn(this);
+});
+
+const toggleProjectsOff = () => {
+    $('.project').each(function() {
         $(this).attr("toggled", "false");
         $(this).find("a").css("pointer-events", "none");
         $(this).find("h4").html("&nbsp;");
         $(this).find("p").html("&nbsp;");
     });
+}
 
-    $(this).find("a").css("pointer-events", "inherit");
-    $(this).attr("toggled") === "true" ? $(this).attr("toggled", "false") : $(this).attr("toggled", "true");
-    let projectName = $(this).attr("id")
+const toggleProjectOn = (project) => {
+    $(project).find("a").css("pointer-events", "inherit");
+    $(project).attr("toggled") === "true" ? $(project).attr("toggled", "false") : $(project).attr("toggled", "true");
+    
+    setProjectText(project);
+}
+
+const setProjectText = (project) => {
+    let projectName = $(project).attr("id")
         .charAt(0).toUpperCase()
-        + $(this).attr("id").slice(1);
+        + $(project).attr("id").slice(1);
     projectName = projectName.replaceAll("-", " ");
+    
     let year = projectName.match(/\d+/g).map(Number);
     projectName = projectName.replace(/\d+/g, "").slice(0, projectName.length - 1);
-    $(this).find("h4").text(projectName);
-    $(this).find("p").text(year);
-});
+
+    $(project).find("h4").text(projectName);
+    $(project).find("p").text(year);
+}
